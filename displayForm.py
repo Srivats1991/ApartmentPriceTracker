dbf80c3ef7c15fe66b2f5b6d3c9ab486c84de6b2
Srivats <sbharad3@masonlivee.gmu.edu>
2019-01-19 01:02:00 -0800

    forgot display form

--- displayForm.py
+++ displayForm.py
@@ -6,12 +6,17 @@ from flask.views import MethodView
 import apartment
 
 class DisplayForm(MethodView):
-	def get(self):
-		"""This function gets all the entries of the recipe model. It uses HTTP GET via get() method.
-		:return: it passes the dictionary to displayForm.html Jinja2 template using render_template.
-		"""
-		model = apartment.get_model()
-		elements = []
-		entries = [dict(address=elements[0],phone=elements[1],beds=elements[2],price=elements[3],title=elements[4], zipcode=elements[5]) for elements in model.select()]
+    def get(self, city, state):
+        """This function gets all the entries of the recipe model. It uses HTTP GET via get() method.
+        :return: it passes the dictionary to displayForm.html Jinja2 template using render_template.
+        """
+        model = apartment.get_model()
+        # city = request.args.get['city']
+        # state = request.args.get['state']
+        city = city
+        state = state
 
-		return render_template('displayForm.html', entries=entries)
+        elements = []
+        entries = [dict(address=elements[0],phone=elements[1],beds=elements[2],price=elements[3],title=elements[4]) for elements in model.select(city, state)]
+        print(entries[0])
+        return render_template('displayForm.html', entries=entries)
