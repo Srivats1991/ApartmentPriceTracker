4e53d19bd0fcb27fc4be49ab980be5b7a28e4460
Srivats <sbharad3@masonlivee.gmu.edu>
2019-01-18 20:27:17 -0800

    proper tables displayed

--- displayForm.py
+++ displayForm.py
@@ -11,7 +11,7 @@ class DisplayForm(MethodView):
 		:return: it passes the dictionary to displayForm.html Jinja2 template using render_template.
 		"""
 		model = apartment.get_model()
+		elements = []
 		entries = [dict(address=elements[0],phone=elements[1],beds=elements[2],price=elements[3],title=elements[4], zipcode=elements[5]) for elements in model.select()]
 
-
-		return render_template('displayForm.html',entries=entries)
+		return render_template('displayForm.html', entries=entries)
